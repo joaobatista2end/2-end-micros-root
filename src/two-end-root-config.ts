de8db472397b0ec1bc17bc874b9dc91950c09239
joaosilvabefore <92741701+joaosilvabefore@users.noreply.github.com>
2024-08-20 12:41:30 -0300

    initial commit

---
+++ src/two-end-root-config.ts
@@ -0,0 +1,17 @@
+import { registerApplication, start, LifeCycles } from 'single-spa';
+
+registerApplication({
+  name: '@two-end/app-dashboard',
+  app: () => System.import<LifeCycles>('@two-end/app-dashboard'),
+  activeWhen: ['/dashboard'],
+});
+
+registerApplication({
+  name: '@two-end/app-planning-poker',
+  app: () => System.import<LifeCycles>('@two-end/app-planning-poker'),
+  activeWhen: ['/planning-poker'],
+});
+
+start({
+  urlRerouteOnly: false,
+});
